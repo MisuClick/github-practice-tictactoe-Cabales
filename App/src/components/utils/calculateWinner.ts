11efbbda2f2c935ab78ec26c878e4f95f623a3fc
Milchzedek Cabales <cabalesmilchzedek@gmail.com>
2025-09-17 18:58:42 +0800

    Edited some problems

--- App/src/components/utils/calculateWinner.ts
+++ App/src/components/utils/calculateWinner.ts
@@ -9,7 +9,7 @@ export function calculateWinner(squares: (string | null)[]) {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  for (let [a, b, c] of lines) {
+  for (const [a, b, c] of lines) {
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }

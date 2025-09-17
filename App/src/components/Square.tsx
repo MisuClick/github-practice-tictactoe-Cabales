e310d5f70306ed45baa688f6778168dd4c22c5d8
Milchzedek Cabales <cabalesmilchzedek@gmail.com>
2025-09-17 20:55:34 +0800

    Edited some design

--- App/src/components/Square.tsx
+++ App/src/components/Square.tsx
@@ -3,11 +3,15 @@ import React from "react";
 interface SquareProps {
   value: string | null;
   onSquareClick: () => void;
+  theme: string;
 }
 
-export default function Square({ value, onSquareClick }: SquareProps) {
+export default function Square({ value, onSquareClick, theme }: SquareProps) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button
+      onClick={onSquareClick}
+      className={`square ${theme}`}
+    >
       {value}
     </button>
   );
